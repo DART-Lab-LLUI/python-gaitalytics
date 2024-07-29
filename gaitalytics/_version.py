524461a08546848ec4dffeb607a1410f2ff672a2
André Böni <andre.boeni@llui.org>
2024-07-29 17:01:54 +0200

    + version file

--- gaitalytics/_version.py
+++ gaitalytics/_version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.2.1.dev0+ge612490.d20240729'
-__version_tuple__ = version_tuple = (0, 2, 1, 'dev0', 'ge612490.d20240729')
+__version__ = version = '0.2.0'
+__version_tuple__ = version_tuple = (0, 2, 0)
