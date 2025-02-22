44a2a86022a336acfc0399f4f01d8a8576c7ceb5
Olivia Ruggaber <olivia.ruggaber@bluewin.ch>
2025-02-22 23:42:54 +0100

    event detection usage example

--- example/detect_events_auto_selection.py
+++ example/detect_events_auto_selection.py
@@ -3,8 +3,10 @@ from gaitalytics import api
 config = api.load_config("./tests/pig_config.yaml")
 trial = api.load_c3d_trial("./tests/test_small.c3d", config)
 
-event_detector = api.get_event_detector("Zen", "Zen", config)
+trial_ref = api.get_ref_from_GRF(trial, config)
+event_detector, user_show = api.find_optimal_detectors(trial_ref, config, method_list=["Zen", "Des", "AC1", "AC6"])
 events = api.detect_events(trial, event_detector)
+print(user_show)
 
 try:
     api.check_events(trial.events)
@@ -13,4 +15,3 @@ except ValueError as e:
 
 api.write_events_to_c3d("./tests/test_small.c3d", events,
                         "./out/test_small_events.c3d")
-
